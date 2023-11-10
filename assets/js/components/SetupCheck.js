d0e5461ca27273d3bbb09f0eefb16ac2dc1f38d3
Mateusz Kleska <m.kleska@telemedi.com>
2023-11-10 14:57:02 +0100

    Add error handling on setup check

--- assets/js/components/SetupCheck.js
+++ assets/js/components/SetupCheck.js
@@ -17,7 +17,10 @@ class SetupCheck extends Component {
         axios.get(`http://telemedi-zadanie.localhost/api/setup-check?testParam=1`).then(response => {
             let responseIsOK = response.data && response.data.testParam === 1
             this.setState({ setupCheck: responseIsOK, loading: false})
-        })
+        }).catch(function (error) {
+            console.error(error);
+            this.setState({ setupCheck: false, loading: false});
+        });
     }
 
     render() {
