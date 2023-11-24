645768eaf607fadbf9583c75e4161557ae566979
Zawoj <zawojskikacperkontakt@gmail.com>
2023-11-24 10:27:05 +0100

    setup typescript and function components

--- assets/js/app.tsx
+++ assets/js/app.tsx
@@ -11,13 +11,15 @@
 // start the Stimulus application
 //import './bootstrap';
 
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "../css/app.css";
+import Home from "./components/Home";
 
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import '../css/app.css';
-import Home from './components/Home';
-
-ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));
-
+ReactDOM.render(
+  <Router>
+    <Home />
+  </Router>,
+  document.getElementById("root")
+);
