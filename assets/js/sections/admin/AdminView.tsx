8560a28edcea6325d9b3f5426f7ec6c404051432
Zawoj <zawojskikacperkontakt@gmail.com>
2023-11-27 16:36:01 +0100

    create separate component for chart

--- assets/js/sections/admin/AdminView.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AdminView = () => {
-  return <div>AdminView</div>;
-};
-
-export default AdminView;
---
+++ assets/js/sections/contanct/ContactView.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactView = () => {
+  return <h1 className="text-center">Contact</h1>;
+};
+
+export default ContactView;
